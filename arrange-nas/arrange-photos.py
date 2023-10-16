8675c17234d25f6b6d185f4a41e93ca22b1bc5eb
Emil Zhai <root@derzh.com>
2023-10-16 21:57:21 +0800

    feat(arrange-photos): use exif info

--- arrange-nas/arrange-photos.py
+++ arrange-nas/arrange-photos.py
@@ -1,15 +1,33 @@
+# pip install Pillow pillow-heif
 import os
 import math
 import pathlib
 import logging
+from PIL import Image, ExifTags
+from pillow_heif import register_heif_opener
 from datetime import datetime
 
 logging.basicConfig(filename='arrange-photos.log', encoding='utf-8', level=logging.DEBUG)
 logging.info('Start: ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+register_heif_opener()
 
 PHOTO_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".tga", ".heic"]
 VIDEO_EXTENSIONS = [".mp4", ".mov"]
 ALL_EXTENSIONS = PHOTO_EXTENSIONS + VIDEO_EXTENSIONS
+
+
+def get_mtime(mtime_file_path):
+    try:
+        image = Image.open(mtime_file_path)
+        image.verify()
+        exif = image.getexif()
+        rtime = exif.get(ExifTags.Base.DateTimeOriginal) or exif.get(ExifTags.Base.DateTime)
+        return (datetime.strptime(rtime, '%Y:%m:%d %H:%M:%S'), "EXIF_CTime")
+    except KeyboardInterrupt as err:
+        raise err
+    except:
+        pass
+    return (datetime.fromtimestamp(os.path.getmtime(mtime_file_path)), "FILE_MTime")
 
 root_path = "."
 for cwd, dirs, files in os.walk(root_path):
@@ -30,7 +48,8 @@ for cwd, dirs, files in os.walk(root_path):
                     break
         if not os.path.isfile(mtime_file_path):
             continue
-        mtime = datetime.fromtimestamp(os.path.getmtime(mtime_file_path))
+
+        (mtime, mtimetype) = get_mtime(mtime_file_path)
         new_filepath_without_ext = os.path.abspath(os.path.join(
             root_path,
             mtime.strftime('%Y') + 'Q' + str(math.floor((int(mtime.strftime('%m')) + 2) / 3)),
@@ -57,13 +76,13 @@ for cwd, dirs, files in os.walk(root_path):
 
         for ext in ALL_EXTENSIONS:
             if os.path.isfile(filepath_without_ext + ext):
-                print("Arrange: " + filepath_without_ext + ext + " => " + new_filepath_without_ext + ext)
+                print("Arrange: " + filepath_without_ext + ext + " => " + new_filepath_without_ext + ext + " (USING " + mtimetype + ")")
                 pardir = os.path.abspath(os.path.join(new_filepath_without_ext, os.pardir))
                 if not os.path.isdir(pardir):
                     logging.info('Makedir: ' + pardir)
                     os.makedirs(pardir)
-                logging.info('Arrange: ' + filepath_without_ext + ext + " => " + new_filepath_without_ext + ext)
-                os.rename(filepath_without_ext + ext, new_filepath_without_ext + ext)
+                logging.info('Arrange: ' + filepath_without_ext + ext + " => " + new_filepath_without_ext + ext + " (USING " + mtimetype + ")")
+                os.rename(filepath_without_ext + ext, new_filepath_without_ext + ext + " (USING " + mtimetype + ")")
 
 for cwd, dirs, files in os.walk(root_path, False):
     for dirname in dirs:
