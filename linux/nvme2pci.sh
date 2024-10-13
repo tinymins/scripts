1d0eaa472789fb8f57c778c2bf1423a63a5c0f41
Emil Zhai <root@derzh.com>
2024-10-13 13:01:48 +0800

    Update nvme2pci.sh

--- linux/nvme2pci.sh
+++ linux/nvme2pci.sh
@@ -10,12 +10,16 @@ for nvme in /sys/class/nvme/nvme*; do
 
     # 如果序列号匹配
     if [ "$serial" == "$target_serial" ]; then
+        # 获取设备的型号
+        model=$(cat "$nvme/model" | tr -d '\n' | xargs)
+
         # 获取设备的符号链接目标
         symlink_target=$(readlink "$nvme")
 
         # 提取完整的PCI地址路径，不包括 /devices 和 /nvme 部分
         pci_address=$(echo "$symlink_target" | sed -n 's|.*\(pci0000:.*\)|\1|p')
 
+        echo "找到的设备型号: $model"
         echo "找到的 PCI 地址路径: $pci_address"
         exit 0
     fi
